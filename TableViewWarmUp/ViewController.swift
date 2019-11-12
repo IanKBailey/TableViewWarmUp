c5dfbb1d8176d329cc287407b25e59ba16db15d1
Ian Bailey <iankbailey2@gmail.com>
2019-11-12 11:12:58 -0500

    Initial Commit

---
+++ TableViewWarmUp/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  TableViewWarmUp
+//
+//  Created by Ian Bailey on 11/12/19.
+//  Copyright © 2019 Ian Bailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    let swiftTopics = ["Basic Operators", "Strings and Characters", "Collection Types", "Control Flow", "Functions",
+    "Closures", "Enumerations", "Structures and Classes", "Properties", "Methods", "Subscripts",
+    "Inheritance", "Initialization", "Deinitialization", "Optional Chaining", "Error Handling",
+    "Type Casting", "Nested Types", "Extensions", "Protocols", "Generics", "Opaque Types",
+    "Automatic Reference Counting", "Memory Safety", "Access Control", "Advanced Operators"
+    ]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+       
+        
+        
+    }
+
+
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return swiftTopics.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "topicCell", for: indexPath)
+        
+        let topics = swiftTopics[indexPath.row]
+        
+        cell.textLabel?.text = topics
+        
+        
+        return cell
+    }
+}
